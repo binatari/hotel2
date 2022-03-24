0283b4f6486e4cb8a7bd6ae575556677346afcf7
binatari <akenebina@gmail.com>
2022-03-24 16:44:56 +0100

    added nav

--- Components/Drawer.js
+++ Components/Drawer.js
@@ -6,7 +6,7 @@ const Drawer = ({show, handleShow}) => {
     <div className={`w-screen h-screen bg-white top-0 left-0 md:hidden z-[100] ${show? 'fixed':'hidden'}`}>
         <ul className='flex flex-col'>
         <li className='flex items-center justify-end'>
-        <button className={` h-12 py-1  md:hidden flex justify-end ${show && 'opened mt-5'}`} onClick={()=>handleShow(!show)}>
+        <button className={`w-10 h-12 py-1  md:hidden flex justify-end ${show && 'opened mt-5'}`} onClick={()=>handleShow(!show)}>
       <svg className='w-full h-full' viewBox="0 0 100 100">
         <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
         <path className="line line2" d="M 20,50 H 80" />
